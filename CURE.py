590741e57406de839a0d239a007940aa7721e29a
vlavrent <73958916+vlavrent@users.noreply.github.com>
2021-05-10 21:32:16 +0300

    Representatives

--- CURE.py
+++ CURE.py
@@ -12,157 +12,115 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import *
 
 
-def shift_towards_centroid(center,rep_points):
+def dist(vecA, vecB):
+        return np.sqrt(np.power(vecA - vecB, 2).sum())
 
-    p = 0.2
-    shifted = defaultdict(list)
+def representatives(data,center,threshold):
+        tempSet = None
 
-    for k,v in rep_points.items():
-        for i in range(len(v)):
-            x = v[i][0] - (p*center[k][0])
-            y = v[i][1] - (p*center[k][1])
-            point = [x,y]
-            shifted[k].append(point)
+        for i in range(1, threshold+1):
+                maxDist = -100
+                maxPoint = None
+                for p in range(0, len(data)):
 
-    return  shifted
+                        if i == 1:
+                                minDist = dist(data[p], center)
+                        else:
+                                X = np.vstack([tempSet,data[p]])
+                                tmpDist = distance.pdist(X)
+                                minDist = tmpDist.min()
+                        if minDist >= maxDist:
+                                maxDist = minDist
+                                maxPoint = data[p]
+                if tempSet is None:
+                        tempSet = maxPoint
+                else:
+                        tempSet = np.vstack((tempSet, maxPoint))
+        for j in range(len(tempSet)):
+                if j==0:
+                        repPoints = None
+                if repPoints is None:
+                        repPoints = tempSet[j,:] + 0.25 * (center - tempSet[j,:])
+                else:
+                        repPoints = np.vstack((repPoints, tempSet[j,:] + 0.25 * (center - tempSet[j,:])))
 
+        return repPoints
 
-
-def representatives(data_array,centers,threshold):
-    centroid = 0
-    point1 = []
-    points_dist = defaultdict(list)
-    new = [[]]
-
-    for i in data_array:
-        maxim = -100
-        max2 = -100
-        row=0
-        #print(i[1])
-        for j in i[1]:
-            x = (j[0]-centers[centroid][0])*(j[0]-centers[centroid][0])
-            y = (j[1]-centers[centroid][1])*(j[1]-centers[centroid][1])
-            dist = math.sqrt(x+y)
-            if maxim<dist:
-                maxim = dist
-                point = [j[0],j[1]]
-                row_del = row
-            row = row +1
-        point1.append(point)
-        points_dist[centroid].append(point)
-
-        row2 = 0
-        for j in i[1]:
-            x = (j[0]-point1[centroid][0])*(j[0]-point1[centroid][0])
-            y = (j[1]-point1[centroid][1])*(j[1]-point1[centroid][1])
-            dist = math.sqrt(x+y)
-            if max2< dist:
-                max2 = dist
-                point2 = [j[0],j[1]]
-                row_del2 = row2
-            row2 = row2+1
-        points_dist[centroid].append(point2)
-        d = np.delete(i[1],(row_del,row_del2),axis=0)
-        new.append(d)
-
-        centroid = centroid + 1
-
-
-    for thres in range(2,threshold):
-        for k,v in points_dist.items():
-            x = 0
-            y = 0
-            max = -100
-            for i in range(len(v)):
-                x = x+v[i][0]
-                y = y+v[i][1]
-            length = len(v)
-            x = x/length
-            y = y/length
-            #print(x,y)
-            #print(v)
-
-            for i in range(len(new[k+1])):
-                ex = (new[k+1][i][0]-x)*(new[k+1][i][0]-x)
-                ey = (new[k+1][i][1]-y)*(new[k+1][i][1]-y)
-                dist = math.sqrt(ex+ey)
-                if max<dist:
-                    max = dist
-                    point = [new[k+1][i][0],new[k+1][i][1]]
-                    row = i
-            points_dist[k].append(point)
-            new[k+1] = np.delete(new[k+1],row,axis=0)
-
-    shifted_centroids = shift_towards_centroid(centers,points_dist)
-    #print(shifted_centroids)
-
-    return shifted_centroids
 
 
 def assign_points(represent_points):
-    def f(x, y):
-        min  = 2000
-        for centroid, representative in represent_points.items():
-            for i in representative:
-                dist = euclidean([x,y],i)
-                if dist<min:
-                    min =dist
-                    c = centroid
-        return c
-    return F.udf(f, IntegerType())
+        def f(x, y):
+                min  = 2000
+                for centroid, representative in represent_points.items():
+                        for i in representative:
+                                for j in i:
+                                        dist = euclidean([x,y],j)
+                                        if dist<min:
+                                                min =dist
+                                                c = centroid
+                return c
+        return F.udf(f, IntegerType())
+
+
+
 
 
 def Cure(path,threshold,k):
-    spark = SparkSession.builder.master("local[*]").appName("kmeans").getOrCreate()
+   spark = SparkSession.builder.master("local[*]").appName("kmeans").getOrCreate()
 
-    df = spark.read.csv(path, header=True).select(F.col("0").cast(spark_types.FloatType()), \
-                                                  F.col("1").cast(spark_types.FloatType()))
+        df = spark.read.csv(path, header=True).select(F.col("0").cast(spark_types.FloatType()), \
+                                                      F.col("1").cast(spark_types.FloatType()))
 
-
-
-    sample = df.sample(False,0.5,12)
-
-    assembler = VectorAssembler(
-        inputCols=["0", "1"],
-        outputCol="features")
-
-    dataset = assembler.transform(sample)
-
-
-    kmeans = BisectingKMeans().setK(k).setSeed(13) \
-        .setFeaturesCol("features") \
-        .setPredictionCol("prediction") \
-        .setDistanceMeasure('euclidean')
-
-    model = kmeans.fit(dataset)
-    predictions = model.transform(dataset)
-
-
-    centers = model.clusterCenters()
+        threshold=4
+        k=3
 
 
 
-    pred = predictions.groupBy('prediction').agg(F.collect_list('features').alias('points'))
-    pred = pred.sort(F.col('prediction').asc())
+        sample = df.sample(False,0.3,7)
 
-    pred.select('points').show()
+        assembler = VectorAssembler(
+                inputCols=["0", "1"],
+                outputCol="features")
+
+        dataset = assembler.transform(sample)
+
+        #total_time = {}
+        silhouette_score ={}
 
 
-    #Collect clustered sample data in main memory
-    data_array = pred.rdd.collect()
+        start = time.time()
+
+        kmeans = BisectingKMeans().setK(k).setSeed(13) \
+                .setFeaturesCol("features") \
+                .setPredictionCol("prediction") \
+                .setDistanceMeasure('euclidean')
+
+        model = kmeans.fit(dataset)
+        predictions = model.transform(dataset)
+        predictions.persist()
+
+        centers = model.clusterCenters()
+
+        pred = predictions.groupBy('prediction').agg(F.collect_list('features').alias('points'))
+
+        pred = pred.sort("prediction")
 
 
-    represent_points = representatives(data_array,centers,threshold)
-    #represent_points = spark.sparkContext.broadcast(represent_points)
-    #represent_points.value
 
+        #Collect clustered sample data in main memory
+        data_array = pred.rdd.collect()
+
+        points_dist = defaultdict(list)
+        for i in range(0,len(centers)):
+                rep_Points = representatives(data_array[i][1],centers[i],threshold)
+                points_dist[i].append(rep_Points.tolist())
 
     df = assembler.transform(df)
     #df.show()
 
 
 
-    prediction_df = df.withColumn("prediction", assign_points(represent_points)(F.col("0"), F.col("1")))
+    prediction_df = df.withColumn("prediction", assign_points(points_dist)(F.col("0"), F.col("1")))
     prediction_df.show()
 
 
