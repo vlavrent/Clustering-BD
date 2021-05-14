4f31a8f4ce293ec15b7b3c20b97e81cbc8905d95
CoGian <kgiantsios@atypon.com>
2021-05-14 14:02:49 +0300

    updates to run with spark submit

--- insert_outliers.py
+++ insert_outliers.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from random import sample
 
+
 def plot_density_estimation(xx, yy, f, xmin, xmax, ymin, ymax, dataset_name):
 	fig = plt.figure(figsize=(8, 8))
 	ax = fig.gca()
