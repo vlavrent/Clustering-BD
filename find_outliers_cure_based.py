bd73656edd936459cd3a1ed816f76cee36365639
CoGian <kgiantsios@atypon.com>
2021-05-15 19:12:26 +0300

    initialized find outliers cure based

--- find_outliers_cure_based.py
+++ find_outliers_cure_based.py
@@ -5,23 +5,51 @@ import argparse
 import numpy as np
 from pyspark.ml.clustering import BisectingKMeans
 import time
-import scipy.stats as st
 from CURE import calc_representatives
+from scipy.spatial import distance
 
 
-def predict_outliers(kernel, mean_representatives_pdf):
+def predict_outliers(represent_points):
     def f(features):
-        if kernel.score_samples(features.reshape(1, -1)) > mean_representatives_pdf:
-            return 1.0
-        else:
-            return 0.0
+
+        min = 2000
+        for centroid, representative in represent_points.items():
+            for i in representative:
+                dist = distance.euclidean(features, i)
+                if dist < min:
+                    min = dist
+                    c = centroid
+
+    return F.udf(f, spark_types.DoubleType())
+
+
+def calc_mean_distance_from_center(centers):
+    def f(prediction, representatives):
+
+        dists = []
+        for representative in representatives:
+            dists.append(distance.euclidean(representative, centers[int(prediction)]))
+
+        return float(np.mean(dists))
+
+    return F.udf(f, spark_types.DoubleType())
+
+
+def calc_std_dev_distance_from_center(centers):
+    def f(prediction, representatives):
+
+        dists = []
+        for representative in representatives:
+            dists.append(distance.euclidean(representative, centers[int(prediction)]))
+
+        return float(np.std(dists))
 
     return F.udf(f, spark_types.DoubleType())
 
 
 def find_outliers(path, threshold, k):
 
-    spark = SparkSession.builder.master("local[*]").appName("find_outliers_kde").getOrCreate()
+    spark = SparkSession.builder.master("local[*]").appName("find_outliers_cure_based").getOrCreate()
 
     all_dataset = spark.read.csv(path, header=True).select(F.col("0").cast(spark_types.FloatType()),
                                                   F.col("1").cast(spark_types.FloatType()),
@@ -47,39 +75,19 @@ def find_outliers(path, threshold, k):
 
     centers = model.clusterCenters()
 
+    print(centers)
     # Find representative points
     pred = predictions.groupBy('prediction').agg(F.collect_list('features').alias('points'))
     pred = pred.sort("prediction")
 
-    representative_points = np.array(pred.withColumn("representatives", calc_representatives(
+    representative_points = pred.withColumn("representatives", calc_representatives(
         centers, threshold)(F.col('points'), F.col('prediction')))\
-        .select(F.explode("representatives")).collect())
-
-    representative_points = representative_points.reshape((representative_points.shape[0], -1))
-
-    X = np.array([[row['0'], row['1']] for row in all_dataset.select(["0", "1"]).collect()])
-    x = X[:, 0]
-    y = X[:, 1]
-    values = np.vstack([x, y])
-    kernel = st.gaussian_kde(values)
-    representatives_pdf = kernel.evaluate(np.array(representative_points.T))
-
-    mean_representatives_pdf = np.mean(representatives_pdf)
-
-    spark.sparkContext.broadcast(kernel)
-    spark.sparkContext.broadcast(mean_representatives_pdf)
-    dataset_with_predicted_outliers = all_dataset\
-        .withColumn("prediction", predict_outliers(kernel, mean_representatives_pdf)(F.col("features")))
-
-    dataset_with_predicted_outliers.persist()
-    num_of_samples = dataset_with_predicted_outliers.count()
-    true_predictions = dataset_with_predicted_outliers.filter(F.col("prediction") == F.col("outlier")).count()
-    dataset_with_predicted_outliers.write.json("dataset_with_predicted_outliers_kde_0.5", mode="overwrite")
-    print("Accuracy: ", true_predictions/num_of_samples)
-
-    # evaluator = BinaryClassificationEvaluator()
-    # evaluator.setLabelCol("outlier").setRawPredictionCol("prediction")
-    # print("AUC ROC:", evaluator.evaluate(dataset_with_predicted_outliers))
+        .select("prediction", "representatives")\
+        .withColumn("mean_dist_center",
+                    calc_mean_distance_from_center(centers)(F.col("prediction"), F.col("representatives")))\
+        .withColumn("std_dev_dist_center",
+                    calc_std_dev_distance_from_center(centers)(F.col("prediction"), F.col("representatives")))
+    representative_points.show(truncate=False)
 
 
 if __name__ == '__main__':
